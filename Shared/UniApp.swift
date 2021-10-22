71cb133ced6ced8aa78c3f44302385664b2b1d8d
Abhik Ahuja <ahujaabhik@protonmail.com>
2021-10-22 14:50:49 -0700

    Initial Commit

---
+++ Shared/UniApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniApp.swift
+//  Shared
+//
+//  Created by Abhik Ahuja on 10/22/21.
+//
+
+import SwiftUI
+
+@main
+struct UniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
